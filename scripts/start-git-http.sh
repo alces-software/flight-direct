fff91c0c18cda005d350897f16f6a717a1e4ced0
william <william.mccumstie@outlook.com>
2018-06-07 17:34:10 +0100

    Setup the apache server to use git
    The installation script now creates a "git" directory that can be
    accessed over http. This uses standard `git-http-backend` library

--- scripts/start-git-http.sh
+++ scripts/start-git-http.sh
@@ -4,5 +4,22 @@
 yum -y update
 yum -y -e0 install httpd git
 
+# Creates the git repo directory
+git_dir='/var/www/git'
+mkdir "$git_dir"
+
+# Sets the git config that will hook into git-http-backend
+cat > /etc/httpd/conf.d/git.conf <<- GitConf
+# Git over HTTP
+<VirtualHost *:80>
+  SetEnv GIT_PROJECT_ROOT $git_dir
+  SetEnv GIT_HTTP_EXPORT_ALL
+  ScriptAlias /git/ /usr/libexec/git-core/git-http-backend/
+
+  <Location /git/>
+  </Location>
+</VirtualHost>
+GitConf
+
 # Starts the apache server
 systemctl start httpd.service
