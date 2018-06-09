8dcba2a3daa3a44b786c0b5d4d996895748e9cd6
william <william.mccumstie@outlook.com>
2018-06-09 11:23:53 +0100

    Add some additional CLI helper functionality
    There is a somewhat standardized way we run our commands. The syntax
    follows a predictable pattern which is now constructed by the
    `default_syntax` method.
    
    Also the name of the command corresponds to the Class name within the
    Commands module. Thus this can be implicitly determined as well.
    
    See comments in the code for more details.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,18 +1,66 @@
 
+require 'active_support/core_ext/string/inflections'
+
 require 'require_all'
 
 require 'commander'
 require 'commands'
 
+##
+# Under development, there are some common features of Commander::Command which
+# I would like to generalize. However until such time that the code stabilized, the
+# new features will be defined here.
+
+class Commander::Command
+  ##
+  # Auto runs all the default methods
+  #
+  def run_with_defaults(*command_args)
+    default_syntax(*command_args)
+    default_action
+  end
+
+  ##
+  # Uses the commands "name" to build the command syntax
+  # "PROGRAM_NAME COMMAND_NAME [ARGUMMENTS] [options]"
+  # The syntax is construct as the following:
+  #   1. It is prefixed by the program(:name) set in the CLI object
+  #   2. Appends the command "name"
+  #   3. Joins the arguments, "args", to the command (this is optional)
+  #   4. Adds the "[options]" suffix which is always requried
+  #
+  def default_syntax(*args)
+    argument_string = args.join(' ') + (args.empty? ? '' : ' ')
+    self.syntax = "#{program(:name)} #{name} #{argument_string}[options]"
+  end
+
+  ##
+  # Hooks into the new "command_module" attribute and appends the command "name"
+  # This then converted to a constant which is used as the command class
+  # The class is initialized with the arguments from Commander and then calls the `run!`
+  # method
+  #
+  def default_action
+    class_str = "#{program(:commands_module)}::#{name.split.map(&:capitalize).join('::')}"
+    klass = class_str.constantize # Ensures the command exists when the CLI is parsed
+    action { |*a| klass.new(*a).run! }
+  end
+
+  private
+
+  def program(*args)
+    Commander::Runner.instance.program(*args)
+  end
+end
+
 module FlightDirect
   class CLI
-    extend Commander::UI
-    extend Commander::UI::AskForClass
     extend Commander::Delegates
 
-    program :name, 'flight-direct'
+    program :name, 'flight'
     program :version, '0.0.0'
     program :description, 'Cluster Management Tool'
+    program :commands_module, 'FlightDirect::Commands'
 
     # global_option('--debug', 'Enables the development mode')
 
@@ -21,21 +69,15 @@ module FlightDirect
     # Display the help if there is no input arguments
     ARGV.push '--help' if ARGV.empty?
 
-    def self.action(command, klass)
-      command.action { |args, opts| klass.new(args, opts).run! }
-    end
-
     command :server do |c|
-      c.syntax = 'flight server [options]'
       c.description = 'Manage the download server'
       c.sub_command_group = true
     end
 
     command :'server create' do |c|
-      c.syntax = 'flight server create [options]'
+      c.run_with_defaults
       c.description = 'Starts the flight direct download server'
       c.hidden = true
-      action(c, FlightDirect::Commands::Server::Start)
     end
   end
 end
