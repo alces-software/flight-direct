ee1071a9343ae3c2a2383b6a355ecddd69efb78b
william <william.mccumstie@outlook.com>
2018-06-10 00:09:38 +0100

    Remove all dependencies from ruby install

--- config/software/ruby.rb
+++ config/software/ruby.rb
@@ -4,14 +4,14 @@ default_version "2.5.1"
 source url: "http://ftp.ruby-lang.org/pub/ruby/ruby-#{version}.tar.gz",
        md5: "23867bc8c16c55e43b14dfe0614bcfa8"
 
-dependency "flight-direct-zlib"
+# dependency "flight-direct-zlib"
 # dependency "ncurses"
 # dependency "openssl"
 
 relative_path "ruby-#{version}"
 
 build do
-  command "./configure"
+  command "./configure --prefix #{install_dir}/opt/ruby"
   command "make"
   command "make install"
 end
