cb01c118c818535290f517c50831e565b4c6ccf0
william <william.mccumstie@outlook.com>
2018-06-10 16:23:35 +0100

    Add code to sync the project libraries in
    Using a omnibus software definition, `flight_direct` can be packaged
    from source.
    
    It requires setting the `source` to the `project_root`

--- config/software/flight_direct.rb
+++ config/software/flight_direct.rb
@@ -1,5 +1,4 @@
 
-
 #
 # Created using the help off:
 # http://blog.scoutapp.com/articles/2013/06/21/omnibus-tutorial-package-a-standalone-ruby-gem
@@ -7,16 +6,22 @@
 # Also  see:
 # https://github.com/scoutapp/omnibus-scout
 #
+# For reference on how to build from source, see `omnibus-gitlab`:
+# https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/config/software/gitlab-cookbooks.rb
+# NOTE: Ignore the `chef-cookbook` stuff for now, that is an extension off `omnibus`
+#
 
-name "flight_diect"
+name "flight_direct"
 # version "5.6.9"
 
 # dependency "ruby"
 # dependency "rubygems"
 
+source path: Omnibus::Config.project_root
+
 build do
   ['bin', 'lib', 'scripts'].each do |sub_dir|
-    sync "#{Omnibus::Config.project_root}/#{sub_dir}/**/*", "#{install_dir}/#{sub_dir}"
+    sync "#{project_dir}/#{sub_dir}/", "#{install_dir}/#{sub_dir}/"
   end
 end
 
