afa0fbee25802fccb393bb4bc7542d6c4e45cee4
william <william.mccumstie@outlook.com>
2018-06-10 18:05:38 +0100

    Trim the excess commented code out

--- config/software/flight_direct.rb
+++ config/software/flight_direct.rb
@@ -14,9 +14,6 @@
 name "flight_direct"
 # version "5.6.9"
 
-# dependency "ruby"
-# dependency "rubygems"
-
 source path: Omnibus::Config.project_root
 
 dependency "ruby"
@@ -32,13 +29,4 @@ build do
   end
   command "cd #{install_dir} && embedded/bin/bundle package --all"
 end
-
-# build do
-#   gem "install scout -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
-#   command "rm -rf /opt/scout/embedded/docs"
-#   command "rm -rf /opt/scout/embedded/share/man"
-#   command "rm -rf /opt/scout/embedded/share/doc"
-#   command "rm -rf /opt/scout/embedded/ssl/man"
-#   command "rm -rf /opt/scout/embedded/info"
-# end
 
