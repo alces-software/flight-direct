a81ee5a6ffc5647e682c68b3224172b81f1e3e23
william <william.mccumstie@outlook.com>
2018-06-14 15:22:01 +0100

    Update the ruby environment variables
    The `GEM_HOME` needs to be a single directory so it acts as a fall back
    location. `GEM_PATH` then needs to add it to the front of it's path so
    it takes priority.
    
    Finally, `flight-direct` should share its gems which have been bundle
    installed. Thus the `BUNDLE_PATH` points to `vendor/cache`

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -1,6 +1,6 @@
 root=$FLIGHT_DIRECT_ROOT
 export PATH=$root/embedded/bin/:$PATH && \
 export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH && \
-export GEM_HOME=$root/embedded/lib/ruby/gems:$GEM_HOME && \
+export GEM_HOME=$root/embedded/lib/ruby/gems && \
 export GEM_PATH=$GEM_HOME:$GEM_PATH && \
-export BUNDLE_PATH=$GEM_HOME
+export BUNDLE_PATH=$root/vendor/cache
