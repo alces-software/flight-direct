390d7f3efc85078b06678c09f55d676364363390
william <william.mccumstie@outlook.com>
2018-06-14 16:01:51 +0100

    Set the FORGE_API_URL if it hasn't already been set
    The hard-coded `API` within forge is incorrect and thus needs to be
    overridden. This can easily be done using a environment variable.
    
    However to maintain the flexibility in the future, it does not override
    previously set values.
    
    Thus an individual machine can change where the API points

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -4,4 +4,7 @@ export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH && \
 export GEM_HOME=$root/embedded/lib/ruby/gems && \
 export GEM_PATH=$GEM_HOME:$GEM_PATH && \
 export BUNDLE_PATH=$root/vendor/cache && \
-export cw_ROOT=$root
+export cw_ROOT=$root && \
+if [ -z "$cw_FORGE_API_URL" ]; then
+  export cw_FORGE_API_URL='https://forge-api.alces-flight.com/v1'
+fi
