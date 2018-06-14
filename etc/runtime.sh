8befe2d26e38d7711365b83533234679c6c2d5b5
william <william.mccumstie@outlook.com>
2018-06-14 15:25:42 +0100

    Alias `cw_ROOT` to be the same as flight direct
    This makes `flight-direct` and stand in for clusterware. Eventually they
    maybe formally integrated

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -3,4 +3,5 @@ export PATH=$root/embedded/bin/:$PATH && \
 export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH && \
 export GEM_HOME=$root/embedded/lib/ruby/gems && \
 export GEM_PATH=$GEM_HOME:$GEM_PATH && \
-export BUNDLE_PATH=$root/vendor/cache
+export BUNDLE_PATH=$root/vendor/cache && \
+export cw_ROOT=$root
