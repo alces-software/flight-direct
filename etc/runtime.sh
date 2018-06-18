ab6e7676ecc756c458b2f7ca516a23d3ae5db3fd
william <william.mccumstie@outlook.com>
2018-06-18 14:32:13 +0100

    Set the clusterware library path
    Clusterware is use to be at the root and thus it can work out its
    function path. However, for the purposes of `flight`, it is installed to
    `opt/clusterware/functions`. (This is the only part of `clusterware`
    that is required by `flight`).
    
    As such the `cw_LIBRARY` path needs to point to this new location

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -6,6 +6,7 @@ export GEM_HOME=$root/embedded/lib/ruby/gems && \
 export GEM_PATH=$GEM_HOME:$GEM_PATH && \
 export BUNDLE_PATH=$root/vendor/gems/ruby/2.5.0 && \
 export cw_ROOT=$root && \
+export cw_LIBPATH=$root/opt/clusterware/functions && \
 if [ -z "$cw_FORGE_API_URL" ]; then
   export cw_FORGE_API_URL='https://forge-api.alces-flight.com/v1'
 fi
