aec0ceeb51f7a6d95e56c38479e6427da2be418f
william <william.mccumstie@outlook.com>
2018-06-18 15:56:45 +0100

    Source the core clusterware functions
    The core clusterware kernel is sourced at runtime. This allows other
    clusterware legacy scripts to "require" the other bash functions.
    
    Note: Not all of the "required" scripts will work with the change of
    "cw_ROOT". However this can be addressed in the future.

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -7,6 +7,7 @@ export GEM_PATH=$GEM_HOME:$GEM_PATH && \
 export BUNDLE_PATH=$root/vendor/gems/ruby/2.5.0 && \
 export cw_ROOT=$root && \
 export cw_LIBPATH=$root/opt/clusterware/functions && \
+source $root/opt/clusterware/clusterware.kernel.sh && \
 if [ -z "$cw_FORGE_API_URL" ]; then
   export cw_FORGE_API_URL='https://forge-api.alces-flight.com/v1'
 fi
