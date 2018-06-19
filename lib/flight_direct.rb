b75077beae94d6b458c1fc283d7f93b2ee585361
william <william.mccumstie@outlook.com>
2018-06-19 16:28:18 +0100

    Allow the ruby root to be moved
    This is similar to what was done previously however the code has been
    cleaned up a bit now.

--- lib/flight_direct.rb
+++ lib/flight_direct.rb
@@ -1,17 +1,20 @@
+fd = 'FLIGHT_DIRECT'
 
 # Sets the load path and development mode from the environment
-development_mode = (ENV['FLIGHT_DIRECT_DEVELOPMENT_MODE'] == 'true')
-load_root = ENV['FLIGHT_DIRECT_ROOT']
+dev_mode = (ENV["#{fd}_DEVELOPMENT_MODE"] == 'true')
+default_root = ENV["#{fd}_ROOT"]
+dev_root = ENV["#{fd}_RUBY_LOAD_ROOT"]
+load_root = (dev_mode && dev_root) ? dev_root : default_root
 
 # Sets up the load paths
-$LOAD_PATH << File.join(load_root, 'lib/flight_direct')
+$LOAD_PATH << File.join(load_root, 'lib', 'flight_direct')
 ENV['BUNDLE_GEMFILE'] = File.join(load_root, 'Gemfile')
 
 # Sets up bundler
 require 'bundler'
 require 'rubygems'
 
-if development_mode
+if dev_mode
   Bundler.setup(:default, :development)
   require 'pry'
 else
