f26544e180d2d84cf05b37a9de53a69e73be402c
william <william.mccumstie@outlook.com>
2018-06-21 14:50:57 +0100

    Put flight's gems `bin` onto the path
    This allows for `flight's` gems to be called without falling back onto
    the system

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -1,9 +1,10 @@
 root=$FLIGHT_DIRECT_ROOT
-export PATH=$root/embedded/bin/:$PATH && \
+vendor_flight=$root/vendor/flight
+export PATH=$root/embedded/bin/:$vendor_flight/bin:$PATH && \
 export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH && \
 export TERMINFO=$root/embedded/lib/terminfo && \
 export GEM_HOME=$root/vendor/share && \
-export GEM_PATH=$GEM_HOME:$root/vendor/flight && \
+export GEM_PATH=$GEM_HOME:$vendor_flight && \
 export BUNDLE_PATH=$GEM_HOME && \
 export cw_ROOT=$root && \
 export cw_LIBPATH=$root/opt/clusterware/functions && \
