37ce383328e7204185bb4343e3ab7f8b35122407
william <william.mccumstie@outlook.com>
2018-06-22 13:23:37 +0100

    Load the clusterware profile into the environment
    The legacy packages require the clusterware `etc/profile.d` scripts to
    be loaded into the environment.
    
    As such a `clusterware-profile` has been created

---
+++ etc/profile.d/clusterware-profile.sh
@@ -0,0 +1,18 @@
+export cw_ROOT=$FLIGHT_DIRECT_ROOT/opt/clusterware
+export cw_LIBPATH=$cw_ROOT/opt/clusterware/functions
+
+# Runs the clusterware profile scripts, this is required by the legacy
+# packages
+if [ -d "$cw_ROOT"/etc/profile.d ]; then
+  for i in "$cw_ROOT"/etc/profile.d/*.sh ; do
+    if [ -r "$i" ]; then
+      if [ "${-#*i}" != "\$-" ]; then
+        . "$i"
+      else
+        . "$i" >/dev/null 2>&1
+      fi
+    fi
+  done
+  unset i
+fi
+
--- etc/runtime.sh
+++ etc/runtime.sh
@@ -6,8 +6,6 @@ export TERMINFO=$root/embedded/lib/terminfo && \
 export GEM_HOME=$root/vendor/share && \
 export GEM_PATH=$GEM_HOME:$vendor_flight && \
 export BUNDLE_PATH=$GEM_HOME && \
-export cw_ROOT=$root/opt/clusterware && \
-export cw_LIBPATH=$cw_ROOT/opt/clusterware/functions && \
 source $root/opt/clusterware/clusterware.kernel.sh && \
 if [ -z "$cw_FORGE_API_URL" ]; then
   export cw_FORGE_API_URL='https://forge-api.alces-flight.com/v1'
