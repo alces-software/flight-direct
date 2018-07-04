21dfd110bcfb91a00f4e8f1b1b0bbf338e92dd18
william <william.mccumstie@outlook.com>
2018-07-04 12:08:28 +0100

    DRY up the profile.d code between flight and cw
    A similar script is used to source both FlightDirect and clusterware's
    profile.d directories. These two scripts have been collapsed into a
    single `profile.sh` file.
    
    This should make it clearer how the profile is set

--- etc/profile.sh
+++ etc/profile.sh
@@ -1,16 +1,26 @@
 # Sets up the `FlightDirect` environment
 export FLIGHT_DIRECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+export cw_ROOT=$FLIGHT_DIRECT_ROOT/opt/clusterware
+
+source_profile_d() {
+  local path="$1"
+  if [ -d $path/etc/profile.d ]; then
+    for i in $path/etc/profile.d/*.sh ; do
+      if [ -r "$i" ]; then
+        if [ "${-#*i}" != "\$-" ]; then
+          . "$i"
+        else
+          . "$i" >/dev/null 2>&1
+        fi
+      fi
+    done
+    unset i
+  fi
+}
 
 # Runs the other files in profile.d
-if [ -d "$FLIGHT_DIRECT_ROOT"/etc/profile.d ]; then
-  for i in "$FLIGHT_DIRECT_ROOT"/etc/profile.d/*.sh ; do
-    if [ -r "$i" ]; then
-      if [ "${-#*i}" != "\$-" ]; then
-        . "$i"
-      else
-        . "$i" >/dev/null 2>&1
-      fi
-    fi
-  done
-  unset i
-fi
+source_profile_d $FLIGHT_DIRECT_ROOT
+source_profile_d $cw_ROOT
+
+# Unsets the helper function
+unset -f source_profile_d
