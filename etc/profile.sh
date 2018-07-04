139e8c6375db48661d44f7ef69aa39a757367221
william <william.mccumstie@outlook.com>
2018-07-04 12:18:41 +0100

    Prefix the helper function with `_fd_`
    The function will still be defined when sourcing the profiles directory
    and thus should have a fairly unique name.

--- etc/profile.sh
+++ etc/profile.sh
@@ -2,7 +2,7 @@
 export FLIGHT_DIRECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 export cw_ROOT=$FLIGHT_DIRECT_ROOT/opt/clusterware
 
-source_profile_d() {
+_fd_source_profile_d() {
   local path="$1"
   if [ -d $path/etc/profile.d ]; then
     for i in $path/etc/profile.d/*.sh ; do
@@ -19,8 +19,8 @@ source_profile_d() {
 }
 
 # Runs the other files in profile.d
-source_profile_d $FLIGHT_DIRECT_ROOT
-source_profile_d $cw_ROOT
+_fd_source_profile_d $FLIGHT_DIRECT_ROOT
+_fd_source_profile_d $cw_ROOT
 
 # Unsets the helper function
-unset -f source_profile_d
+unset -f _fd_source_profile_d
