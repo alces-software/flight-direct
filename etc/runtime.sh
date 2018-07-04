585c6dde67e5a5ad44a76229c64cd7c520c5f45b
william <william.mccumstie@outlook.com>
2018-07-04 11:37:55 +0100

    Use `set -e` instead of the `&& \`
    It amounts to the same thing, however it is easier to maintain

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -1,9 +1,10 @@
+set -e
 root=$FLIGHT_DIRECT_ROOT
 vendor_flight=$root/vendor/flight
-export PATH=$root/embedded/bin/:$vendor_flight/bin:$PATH && \
-export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH && \
-export TERMINFO=$root/embedded/lib/terminfo && \
-export GEM_HOME=$root/vendor/share && \
-export GEM_PATH=$GEM_HOME:$vendor_flight && \
-export BUNDLE_PATH=$GEM_HOME && \
+export PATH=$root/embedded/bin/:$vendor_flight/bin:$PATH
+export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH
+export TERMINFO=$root/embedded/lib/terminfo
+export GEM_HOME=$root/vendor/share
+export GEM_PATH=$GEM_HOME:$vendor_flight
+export BUNDLE_PATH=$GEM_HOME
 source $root/opt/clusterware/lib/clusterware.kernel.sh
