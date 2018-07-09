7fafdaa033788c7cd4c9a6184d55fddfe12f89ab
william <william.mccumstie@outlook.com>
2018-07-09 16:34:40 +0100

    Do not set the bundle path in the runtime
    This can cause issues with running gems on a different path

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -9,7 +9,6 @@ export LD_LIBRARY_PATH=$root/embedded/lib:$LD_LIBRARY_PATH
 export TERMINFO=$root/embedded/lib/terminfo
 export GEM_HOME=$root/vendor/share
 export GEM_PATH=$GEM_HOME:$vendor_flight
-export BUNDLE_PATH=$GEM_HOME
 export SSL_CERT_FILE=$root/embedded/ssl/certs/cacert.pem
 source $cw_ROOT/lib/clusterware.kernel.sh
 
