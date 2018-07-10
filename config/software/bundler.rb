4e9a94ca8738af532a35dc13a64b6100d29841d8
william <william.mccumstie@outlook.com>
2018-07-10 16:31:32 +0100

    Build `Bundler` within the same gem env
    Bundler also needs to be installed into the `vendor` directory. This
    makes the gem environment more consistent.
    
    Sorry if this causes your future bug

---
+++ config/software/bundler.rb
@@ -0,0 +1,40 @@
+#
+# Copyright 2012-2018, Chef Software Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "bundler"
+
+license "MIT"
+license_file "https://raw.githubusercontent.com/bundler/bundler/master/LICENSE.md"
+skip_transitive_dependency_licensing true
+
+dependency "rubygems"
+
+build do
+  gem_home = "#{install_dir}/vendor/share"
+  mkdir gem_home
+  env = with_standard_compiler_flags(with_embedded_path({
+    "GEM_HOME" => gem_home,
+    "GEM_PATH" => gem_home,
+    "BUNDLE_PATH" => gem_home
+  }))
+
+  v_opts = "--version '#{version}'" unless version.nil?
+  gem [
+    "install bundler",
+    v_opts,
+    "--no-ri --no-rdoc --force",
+  ].compact.join(" "), env: env
+end
