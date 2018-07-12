675c0e29074144ff14933f333eb6cf93b7819ed8
william <william.mccumstie@outlook.com>
2018-07-12 19:11:36 +0100

    Run the module command in the current shell
    The purpose of the `module` command is to alter the current shell. Thus
    it needs to run in the current shell (and not subshell).
    
    To achieve this, the `module` calls `modulecmd` that only returns the
    string to be evaled.

--- etc/profile.d/base.sh
+++ etc/profile.d/base.sh
@@ -7,15 +7,34 @@
 
 # The delimiter is defined with interpolation so it does not get subbed
 # when the env is being parsed
+
 flight() {
-  ( set -e
-    unset FLIGHT_DIRECT_ENV_BACKUP
-    delim=":$(echo 'FD_DELIM'):"
-    export FLIGHT_DIRECT_ENV_BACKUP="$(env -0 | sed "s/\x0/$delim/g")"
-    source "$FLIGHT_DIRECT_ROOT"/etc/runtime.sh
-    cd "$FLIGHT_DIRECT_ROOT"
-    bin/flight "$@"
-  )
+  _flight() {
+    ( set -e
+      unset FLIGHT_DIRECT_ENV_BACKUP
+      delim=":$(echo 'FD_DELIM'):"
+      export FLIGHT_DIRECT_ENV_BACKUP="$(env -0 | sed "s/\x0/$delim/g")"
+      source "$FLIGHT_DIRECT_ROOT"/etc/runtime.sh
+      cd "$FLIGHT_DIRECT_ROOT"
+      bin/flight "$@"
+    )
+  }
+
+  # The module command needs to be `eval'd` within the current shell
+  # Thus it polls `flight modulecmd` for the string and then runs it
+  # It only matches shorthand between `mo-module`
+  if [[ $1 == 'mo'* && 'module' == "$1"* ]]; then
+    shift 1
+    if module_env=$(_flight 'modulecmd' "$@"); then
+      eval $module_env
+    else
+      echo 'An error has occurred running module' >&2
+      echo "$module_env" >&2
+    fi
+  else
+    _flight "$@"
+  fi
+  unset _flight
 }
 export -f flight
 
