cf5b4d1fa5445fc64e9e0b83fc7e8bed6a051a4a
william <william.mccumstie@outlook.com>
2018-07-12 13:25:08 +0100

    Port the env setup from `00-base.sh`
    These env vars where orginally set by:
    https://github.com/alces-software/clusterware/blob/master/etc/profile.d/00-base.sh

--- etc/runtime.sh
+++ etc/runtime.sh
@@ -9,14 +9,24 @@ export TERMINFO=$root/embedded/lib/terminfo
 export GEM_HOME=$root/vendor/share
 export GEM_PATH=$GEM_HOME
 export SSL_CERT_FILE=$root/embedded/ssl/certs/cacert.pem
-source $cw_ROOT/lib/clusterware.kernel.sh
 
 # Source the distribution specific runtime environment
 source $root/etc/dist-runtime.sh
 
+# Sets up clusterware
+source $cw_ROOT/lib/clusterware.kernel.sh
+if [[ -t 1 && "$TERM" != linux ]]; then
+    export cw_COLOUR=1
+else
+    export cw_COLOUR=0
+fi
+export cw_SHELL=bash
+
 # Adds support for running the legacy gridware package
 kernel_load() { source "${cw_ROOT}/lib/clusterware.kernel.sh"; }
 cw_RUBY_EXEC() { exec ruby $0 "$@"; }
+
+# TODO: Make these 'zsh' compatible
 export -f kernel_load
 export -f cw_RUBY_EXEC
 
