e96d10a6c3a421a92b255be3852ff89df23057da
william <william.mccumstie@outlook.com>
2018-07-16 15:48:49 +0100

    Display the motd after the banner is loaded

--- etc/profile.d/banner.sh
+++ etc/profile.d/banner.sh
@@ -2,13 +2,32 @@
 # Runs the moose-bird in a sub-shell. The animation can exit early which crashes the
 # current shell. Instead it is ran in a sub-shell which prevents its failure from affecting
 # the rest of the setup
-
-_run_flight_banner() {
+_run_moosebird() {
   local version="FlightDirect $(flight version)"
   local key='PRETTY_NAME='
   local dist=$(cat /etc/os-release | grep $key | tr -d $key | tr -d '"')
   (. "$FL_ROOT"/scripts/moosebird.sh 'TODO_CLUSTERNAME' "$version" "$dist")
 }
-_run_flight_banner
-unset _run_flight_banner
+_print_motd() {
+  local i
+  if [ -d "$FL_ROOT"/etc/motd.d ]; then
+    # Text and scripts are globed together to ensure they are run in
+    # the correct order
+    for i in $(ls "$FL_ROOT"/etc/motd.d/*.{sh,txt}) ; do
+      if [ -r "$i" ]; then
+        # Cat text files
+        if [ "${i: -4}" == '.txt' ]; then
+          cat "$i" | sed '/^#/ d'
 
+        # Runs sh files NOTE: They are cat'd to avoid permissions issues
+        elif [ "${i: -3}" == '.sh' ]; then
+          cat $i | bash | sed '/^#/ d'
+        fi
+      fi
+    done
+  fi
+}
+_run_moosebird
+_print_motd
+unset _run_moosebird
+unset _print_motd
