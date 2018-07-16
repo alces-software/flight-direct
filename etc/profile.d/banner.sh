ec58606a80554260c765bb62492b0ce3cadf825a
william <william.mccumstie@outlook.com>
2018-07-16 14:13:37 +0100

    Add TODO to the banner script
    These will be replaced in the future

--- etc/profile.d/banner.sh
+++ etc/profile.d/banner.sh
@@ -3,4 +3,4 @@
 # current shell. Instead it is ran in a sub-shell which prevents its failure from affecting
 # the rest of the setup
 
-(. "$FL_ROOT"/scripts/moosebird.sh)
+(. "$FL_ROOT"/scripts/moosebird.sh 'TODO_CLUSTERNAME', 'TODO_VERSION', 'TODO_DISTRO')
