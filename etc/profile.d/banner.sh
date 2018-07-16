15c0448a6fc92006ec576125b1e0d89dd63b6db8
william <william.mccumstie@outlook.com>
2018-07-16 14:52:43 +0100

    Hook the FlightDirect version into the banner

--- etc/profile.d/banner.sh
+++ etc/profile.d/banner.sh
@@ -3,4 +3,6 @@
 # current shell. Instead it is ran in a sub-shell which prevents its failure from affecting
 # the rest of the setup
 
-(. "$FL_ROOT"/scripts/moosebird.sh 'TODO_CLUSTERNAME', 'TODO_VERSION', 'TODO_DISTRO')
+version="FlightDirect $(flight version)"
+(. "$FL_ROOT"/scripts/moosebird.sh 'TODO_CLUSTERNAME' "$version" 'TODO_DISTRO')
+unset version
