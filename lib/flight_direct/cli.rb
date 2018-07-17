6d7e90a3e4a6ca4044b0ffaea342a4263677eb4e
william <william.mccumstie@outlook.com>
2018-07-17 13:04:15 +0100

    Only extract the `synopsis` from the action files
    The command name is now inferred from the file path not from internal
    within the file. Also the name and path as set after the loop so they
    can not be overridden

--- lib/flight_direct/cli.rb
+++ lib/flight_direct/cli.rb
@@ -24,19 +24,22 @@ module FlightDirect
 
       # Extracts the info block contained at the top of the action files
       def extract_info(path)
-        cmd = OpenStruct.new(path: path)
+        cmd = OpenStruct.new
         File.read(path).each_line.map(&:chomp).each do |line|
           # Only match lines that start with `: `
           # However skip any lines that start with `: '`
           # The loop is stopped once the name and synopsis have been set
-          break if cmd.name && cmd.synopsis
+          break if cmd.synopsis
           next unless /\A#?:\s(?!').*:\s.*/.match?(line)
           delim = (line[0] == '#' ? '#:' : ':')
           label = /(?<=\A#{delim}\s).*(?=:)/.match(line)[0]
           data = /(?<=\A#{delim}\s#{label}:\s).*/.match(line)[0]
           cmd[label.downcase.to_sym] = data
         end
-        cmd
+        cmd.tap do |x|
+          x[:path] = path
+          x[:name] = File.basename(path, '.*')
+        end
       end
     end
 
