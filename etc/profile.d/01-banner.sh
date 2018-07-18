8d627ac1a1dacd424caa7860ee435d33cb1019bd
william <william.mccumstie@outlook.com>
2018-07-18 20:47:18 +0100

    Extract a helper method for pulling configs
    This is a common requirement in the profiles as it allows for users
    to modify the profile behaviour.
    
    However using ruby is quite slow, so instead a helper method has been
    defined

--- etc/profile.d/01-banner.sh
+++ etc/profile.d/01-banner.sh
@@ -2,15 +2,10 @@
 # Runs the moose-bird in a sub-shell. The animation can exit early which crashes the
 # current shell. Instead it is ran in a sub-shell which prevents its failure from affecting
 # the rest of the setup
-_fl_clustername() {
-  local config=$(source "$FL_ROOT"/var/flight.conf 2>/dev/null
-                 echo $FL_CONFIG_CLUSTERNAME )
-  echo ${config:-'Unconfigured'}
-}
 _fl_run_moosebird() {
   local version="Flight Direct $(flight version)"
   local dist=$(. /etc/os-release; echo $PRETTY_NAME)
-  local name=$(_fl_clustername)
+  local name=$(_fl_helper_config_get CLUSTERNAME)
   (. "$FL_ROOT"/scripts/moosebird.sh "$name" "$version" "$dist")
 }
 _fl_print_motd() {
@@ -36,4 +31,3 @@ _fl_run_moosebird
 _fl_print_motd
 unset _fl_run_moosebird
 unset _fl_print_motd
-unset _fl_clustername
--- etc/profile.sh
+++ etc/profile.sh
@@ -1,3 +1,4 @@
+set +e
 # Do not source any files if already sourced
 if [[ -z "${FL_SOURCED}" ]]; then
   export FL_SOURCED=true
@@ -22,10 +23,25 @@ if [[ -z "${FL_SOURCED}" ]]; then
           else
             . "$i" >/dev/null 2>&1
           fi
+          # Ensure no flight profile leave errors on
+          set +e
         fi
       done
       unset i
     fi
+  }
+
+  # This allows profile scripts to poll for config values
+  # without booting up ruby. This about 50x faster than running
+  # `flight config get`
+  # NOTE: THIS IS A DUMB TOOL! The input must be in capitals
+  _fl_helper_config_get() {
+    echo $(
+      var="FL_CONFIG_$1"
+      set -a +e
+      source "$FL_ROOT"/var/flight.conf 2>/dev/null
+      echo ${!var}
+    )
   }
 
   # Runs the other files in profile.d
@@ -33,7 +49,5 @@ if [[ -z "${FL_SOURCED}" ]]; then
 
   # Unsets the helper function
   unset -f _fl_source_profile_d
-
-  # Ensure that errors don't crash the shell
-  set +e
+  unset -f _fl_helper_config_get
 fi
