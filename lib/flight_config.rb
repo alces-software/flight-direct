6d1890c99df15d67ba0a504108bd7d4f9b5b5bfc
william <william.mccumstie@outlook.com>
2018-07-25 12:58:33 +0100

    Add a flag that errors if the env is missing
    By default, the `get` command will return nil if the env is missing.
    However quite often a missing env is a error condition. To simplify this
    process, an `allow_missing` flag has been added. When false, it will
    error if the env is missing

--- lib/flight_config.rb
+++ lib/flight_config.rb
@@ -3,8 +3,13 @@ module FlightConfig
   PREFIX = 'FL_CONFIG_'.freeze
 
   class << self
-    def get(key)
-      ENV[key_to_env(key)]
+    def get(key, allow_missing: true)
+      env = key_to_env(key)
+      ENV[env].tap do |value|
+        raise <<-ERROR if !(allow_missing || value)
+'#{env}' has not been set. Please see 'flight config set' for more details
+ERROR
+      end
     end
 
     def key_to_env(key)
