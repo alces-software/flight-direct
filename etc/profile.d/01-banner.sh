d150697adf80381305fda34d90a65e1f959d2b7b
william <william.mccumstie@outlook.com>
2018-08-09 15:51:07 +0100

    Add a csh banner script that wraps the bash one
    The csh banner script sources the bash script. The bash script needed to
    be updated to ensure it has the bash `flight` function

--- etc/profile.d/01-banner.sh
+++ etc/profile.d/01-banner.sh
@@ -3,6 +3,12 @@
 # current shell. Instead it is ran in a sub-shell which prevents its failure from affecting
 # the rest of the setup
 
+# This script is ran by the `csh` banner script. Thus the base profile script
+# might need to be sourced
+if [ -z "$BASH_FUNC_flight" ]; then
+  source $FL_ROOT/etc/profile.d/00-base.sh
+fi
+
 # This allows profile scripts to poll for config values
 # without booting up ruby. This about 50x faster than running
 # `flight config get`
